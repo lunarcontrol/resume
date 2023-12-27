bd27dec5a62230171c4e7859708ad9f0a127cc4e
Royce <roycewhitaker@rocketmail.com>
2023-12-27 14:07:05 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -138,6 +138,7 @@ export default function Page() {
                 <CardHeader>
                   <div className="flex items-center justify-between gap-x-2 text-base">
                     <h3 className="font-semibold leading-none">
+                      <EducationIcon />
                       {education.school}
                     </h3>
                     <div className="text-sm tabular-nums text-gray-500">
